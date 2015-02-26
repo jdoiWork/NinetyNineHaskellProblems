867f57c798571ee9bbe72f65128a0e0fc750a6b6
jdoiWork <jdoi.work@gmail.com>
2015-02-27 01:13:36 +0900

    問02追加

--- src/Problem/Q01To10.hs
+++ src/Problem/Q01To10.hs
@@ -1,5 +1,10 @@
 module Problem.Q01To10
   ( myLast
+  , myButLast
   ) where
 
+myLast :: [a] -> a
 myLast = head . reverse
+
+myButLast :: [a] -> a
+myButLast = head . tail . reverse
--- test/Problem/Q01To10Spec.hs
+++ test/Problem/Q01To10Spec.hs
@@ -12,3 +12,8 @@ spec = do
     it "Find the last element of a list." $ do
       myLast [1,2,3,4]     `shouldBe` (4 :: Int)
       myLast ['x','y','z'] `shouldBe` 'z'
+
+  describe "Q02" $ do
+    it "Find the last but one element of a list." $ do
+      myButLast [1,2,3,4]     `shouldBe` (3 :: Int)
+      myButLast ['x','y','z'] `shouldBe` 'y'
